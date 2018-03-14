7ec0dd773c381aabd7e2a2dfc9b9504227755f2d
WyinKok <wyin.kwy@gmail.com>
2018-03-14 17:29:39 +0800

    Created glossary page (system test not implemented yet)

---
+++ src/test/java/guitests/guihandles/GlossaryWindowHandle.java
@@ -0,0 +1,34 @@
+package guitests.guihandles;
+
+import java.net.URL;
+
+import guitests.GuiRobot;
+import javafx.stage.Stage;
+
+/**
+ * A handle to the {@code GlossaryWindow} of the application.
+ */
+public class GlossaryWindowHandle extends StageHandle {
+
+    public static final String GLOSSARY_WINDOW_TITLE = "Glossary";
+
+    private static final String GLOSSARY_WINDOW_BROWSER_ID = "#browser";
+
+    public GlossaryWindowHandle(Stage glossaryWindowStage) {
+        super(glossaryWindowStage);
+    }
+
+    /**
+     * Returns true if a help window is currently present in the application.
+     */
+    public static boolean isWindowPresent() {
+        return new GuiRobot().isWindowShown(GLOSSARY_WINDOW_TITLE);
+    }
+
+    /**
+     * Returns the {@code URL} of the currently loaded page.
+     */
+    public URL getLoadedUrl()  {
+        return WebViewUtil.getLoadedUrl(getChildNode(GLOSSARY_WINDOW_BROWSER_ID));
+    }
+}
---
+++ src/test/java/seedu/address/ui/GlossaryWindowTest.java
@@ -0,0 +1,33 @@
+package seedu.address.ui;
+
+import static org.junit.Assert.assertEquals;
+import static seedu.address.ui.GlossaryWindow.USERGUIDE_FILE_PATH;
+
+import java.net.URL;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.testfx.api.FxToolkit;
+
+import guitests.guihandles.GlossaryWindowHandle;
+import javafx.stage.Stage;
+
+public class GlossaryWindowTest extends GuiUnitTest {
+
+    private GlossaryWindow glossaryWindow;
+    private GlossaryWindowHandle glossaryWindowHandle;
+
+    @Before
+    public void setUp() throws Exception {
+        guiRobot.interact(() -> glossaryWindow = new GlossaryWindow());
+        Stage glossaryWindowStage = FxToolkit.setupStage((stage) -> stage.setScene(glossaryWindow.getRoot().getScene()));
+        FxToolkit.showStage();
+        glossaryWindowHandle = new GlossaryWindowHandle(glossaryWindowStage);
+    }
+
+    @Test
+    public void display() {
+        URL expectedGlossaryPage = GlossaryWindow.class.getResource(USERGUIDE_FILE_PATH);
+        assertEquals(expectedGlossaryPage, glossaryWindowHandle.getLoadedUrl());
+    }
+}
