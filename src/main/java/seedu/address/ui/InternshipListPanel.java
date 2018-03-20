1482144a11fd490b7127efc91dfc2abac27ae11a
WyinKok <wyin.kwy@gmail.com>
2018-03-20 13:37:53 +0800

    update lexicon

--- src/main/java/seedu/address/ui/InternshipListPanel.java
+++ src/main/java/seedu/address/ui/InternshipListPanel.java
@@ -13,8 +13,8 @@ import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
-import seedu.address.commons.events.ui.JumpToListRequestEvent;
 import seedu.address.commons.events.ui.InternshipPanelSelectionChangedEvent;
+import seedu.address.commons.events.ui.JumpToListRequestEvent;
 import seedu.address.model.person.Person;
 
 /**
