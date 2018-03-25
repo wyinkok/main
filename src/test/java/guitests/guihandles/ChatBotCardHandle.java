ec52e755dea50421c7db5736ada2761b974592d5
WyinKok <wyin.kwy@gmail.com>
2018-03-25 15:01:47 +0800

    added chatbotcard tests and handle

---
+++ src/test/java/guitests/guihandles/ChatBotCardHandle.java
@@ -0,0 +1,37 @@
+package guitests.guihandles;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Region;
+
+/**
+ * Provides a handle to a chatbot card in the chat bot panel.
+ */
+
+public class ChatBotCardHandle extends NodeHandle<Node> {
+    private static final String USERNAME_FIELD_ID = "#username";
+    private static final String MESSAGES_FIELD_ID = "#messages";
+
+    private final Label usernameLabel;
+    private final Label messagesLabel;
+
+    public ChatBotCardHandle(Node cardNode) {
+        super(cardNode);
+
+        this.usernameLabel = getChildNode(USERNAME_FIELD_ID);
+        this.messagesLabel = getChildNode(MESSAGES_FIELD_ID);
+    }
+
+    public String getUsername(){
+        return usernameLabel.getText();
+    }
+
+    public String getMessages() {
+        return messagesLabel.getText();
+    }
+
+}
+
