ff6b81b0109e0ffbff169e26911130122a8d6994
Colin Ng <colinn94@gmail.com>
2018-04-06 02:03:14 +0800

    Checkstyle

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -26,10 +26,10 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.internship.Internship;
 import seedu.address.model.internship.exceptions.DuplicateInternshipException;
 import seedu.address.model.internship.exceptions.InternshipNotFoundException;
-import seedu.address.model.util.Sorter;
 import seedu.address.model.internship.exceptions.SavedTagNotFoundException;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.tag.UniqueTagList;
+import seedu.address.model.util.Sorter;
 
 /**
  * Represents the in-memory model of the address book data.
