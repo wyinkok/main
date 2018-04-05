74d76b904ae74032e6d60ffb0c0be4aa8f779899
Colin Ng <colinn94@gmail.com>
2018-04-06 00:04:54 +0800

    Remove unused import

--- src/main/java/seedu/address/model/internship/Internship.java
+++ src/main/java/seedu/address/model/internship/Internship.java
@@ -8,7 +8,6 @@ import java.util.Set;
 
 import seedu.address.model.tag.Tag;
 import seedu.address.model.tag.UniqueTagList;
-import sun.jvm.hotspot.utilities.AssertionFailure;
 
 /**
  * Represents a Internship in the internship book.
