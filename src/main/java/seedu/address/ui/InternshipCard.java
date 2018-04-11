43d28d85816b89c34ac76a5c2fde73cabd7b83c2
TanCiKang <35593675+e0052813@users.noreply.github.com>
2018-04-11 17:53:00 +0800

    edit for error

--- src/main/java/seedu/address/ui/InternshipCard.java
+++ src/main/java/seedu/address/ui/InternshipCard.java
@@ -43,8 +43,6 @@ public class InternshipCard extends UiPart<Region> {
     @FXML
     private Label salary;
     @FXML
-    private Label address;
-    @FXML
     private FlowPane tags;
 
     public InternshipCard(Internship internship, int displayedIndex) {
@@ -58,7 +56,6 @@ public class InternshipCard extends UiPart<Region> {
         role.setText(internship.getRole().value);
         salaryTitle.setText("Stipend: $");
         salary.setText(internship.getSalary().value);
-        address.setText(internship.getAddress().value);
 
         internship.getTags().forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
     }
