4790af6b0f8c4d5c313384217f1bd02455fa7ba0
Colin Ng <colinn94@gmail.com>
2018-04-11 17:38:51 +0800

    Update Message for Start

--- src/main/java/seedu/address/logic/commands/StartCommand.java
+++ src/main/java/seedu/address/logic/commands/StartCommand.java
@@ -13,11 +13,10 @@ public class StartCommand extends Command {
 
     public static final String MESSAGE_START_ACKNOWLEDGEMENT = "Next, please key in all the industries and roles"
             + " that you are interested in.  To view the full list of possible industries and roles "
-            + "key in 'help' \n\nE.g  search healthcare technology dataanalytics humanresource ";
+            + "key in 'help' \n\nE.g  search healthcare technology engineering marketing consulting";
     @Override
     public CommandResult execute() {
         EventsCenter.getInstance().post(new StartAppRequestEvent());
         return new CommandResult(MESSAGE_START_ACKNOWLEDGEMENT);
     }
-
 }
