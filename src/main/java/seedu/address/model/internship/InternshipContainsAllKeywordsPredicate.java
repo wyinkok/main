6948fb7abccd9752dc6992998ed9a95fd3bb6f51
Colin Ng <colinn94@gmail.com>
2018-04-14 19:19:03 +0800

    Update internshipAttributeString method

--- src/main/java/seedu/address/model/internship/InternshipContainsAllKeywordsPredicate.java
+++ src/main/java/seedu/address/model/internship/InternshipContainsAllKeywordsPredicate.java
@@ -30,7 +30,7 @@ public class InternshipContainsAllKeywordsPredicate implements Predicate<Interns
     private String internshipAttributeString(Internship internship) {
         // tags currently toString as [tagName], replace [] with whitespace for searching.
         // Also replaces commas with whitespace
-        return internship.toString().replaceAll("[\\[+\\]+\\,]", " "));
+        return internship.toString().replaceAll("[\\[+\\]+\\,]", " ");
     }
 
     @Override
