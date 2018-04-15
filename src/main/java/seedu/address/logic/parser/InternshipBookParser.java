e308a4a054e169cb8c222fb3ca78bb4a94a5efc9
WyinKok <wyin.kwy@gmail.com>
2018-04-15 22:39:19 +0800

    checkstyle

--- src/main/java/seedu/address/logic/parser/InternshipBookParser.java
+++ src/main/java/seedu/address/logic/parser/InternshipBookParser.java
@@ -114,7 +114,6 @@ public class InternshipBookParser {
             checkIfContainArguments(arguments);
             hasRestarted = false;
             checkIfConversationRestarted();
-            
         case NewChatCommand.COMMAND_WORD:
             checkIfContainArguments(arguments);
             hasStarted = false;
