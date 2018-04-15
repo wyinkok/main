48a5792af27c443c6d9504bae5a5d5fef3ad21d2
Colin Ng <colinn94@gmail.com>
2018-04-15 21:59:13 +0800

    Edit Checkstyle

--- src/test/java/systemtests/FilterCommandSystemTest.java
+++ src/test/java/systemtests/FilterCommandSystemTest.java
@@ -18,7 +18,7 @@ import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.RedoCommand;
 import seedu.address.logic.commands.UndoCommand;
 import seedu.address.model.Model;
-i
+
 import seedu.address.model.internship.exceptions.DuplicateInternshipException;
 import seedu.address.model.internship.exceptions.InternshipNotFoundException;
 
